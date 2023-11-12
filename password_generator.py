d7cf474c7ce9353d9db80b7d7680ac4593b2b9e0
windblows123 <iaroslavtsevaleksandr@gmail.com>
2023-11-12 14:26:03 +0400

    Commiting the password generator

---
+++ password_generator.py
@@ -0,0 +1,26 @@
+from random import choices, shuffle, choice
+
+digits = '0123456789'
+lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
+uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+nonchars = 'Il1o0O'
+
+chars = lowercase_letters + uppercase_letters + digits
+for c in nonchars:
+    chars = chars.replace(c, '')
+
+
+def generate_password(length, chars):
+    password = ''
+    password += choice([x for x in chars if x.islower()])
+    password += choice([x for x in chars if x.isupper()])
+    password += choice([x for x in chars if x.isdigit()])
+    password += ''.join(choices(chars, k = length - 3))
+    password = list(password)
+    shuffle(password)
+    password = ''.join(password)
+    return password
+
+amount_of_passwords, length = int(input('Введите количество паролей\n')), int(input('Введите длину пароля\n'))
+for _ in range(amount_of_passwords):
+    print(generate_password(length, chars))
